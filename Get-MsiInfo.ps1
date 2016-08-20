934cf3551fbc82ad1f95de03955ca9f68ebf3627
MLauper <laupm4@bfh.ch>
2016-08-20 13:39:51 +0200

    Correct Typo

--- Get-MsiInfo.ps1
+++ Get-MsiInfo.ps1
@@ -3,12 +3,12 @@ function Get-MsiInfo {
   .SYNOPSIS 
   Get Summary Info from MSI. 
   .DESCRIPTION 
-  This function retrieves the  
+  This function retrieves the  summary informations from a MSI package.
   All Values are described here: https://msdn.microsoft.com/en-us/library/windows/desktop/aa372045(v=vs.85).aspx
   .EXAMPLE 
   Get-MsiInfo "C:\Program Files (x86)\Windows Kits\10\bin\x86\Orca-x86_en-us.msi"
   .EXAMPLE 
-  et-MsiInfo -MsiFilePath "C:\Program Files (x86)\Windows Kits\10\bin\x86\Orca-x86_en-us.msi"
+  Get-MsiInfo -MsiFilePath "C:\Program Files (x86)\Windows Kits\10\bin\x86\Orca-x86_en-us.msi"
   .PARAMETER MsiFilePath 
   The Msi from which the information is retrieved.  
   #> 
